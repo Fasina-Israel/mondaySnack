88289feef2c0b7da7fe789ce3543ee4294d41b6d
Fasina Israel <Fashman28@gmail.com>
2021-08-09 05:03:44 +0100

    fash

---
+++ fourPointTwoTwo.java
@@ -0,0 +1,22 @@
+public class fourPointTwoTwo{ 
+
+	public static void main(String[] args) {
+        int n = 1;
+
+        int tens = 10;
+
+        System.out.println("N 10*N 100*N 1000*N");
+        for (; n <= 5; n++) {
+         int ten = n * 10;
+         int hundred = n * 100;
+         int thousand = n * 1000;
+
+            System.out.println(n + "  "+ ten + "   "+ hundred + "  " + thousand);
+
+
+
+
+        }
+
+	}
+        }
